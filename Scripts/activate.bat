69bc92684d3b02f657a14d54d41375165e191758
zk-313 <zulfiqar991@hotmail.com>
2024-10-08 16:44:11 -0400

    Final Django commit

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=Z:\git\Django-chat-app\django-chat-app
+set VIRTUAL_ENV=Z:\git\Django-chat-app
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(django-chat-app) %PROMPT%
+set PROMPT=(Django-chat-app) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(django-chat-app) 
+set VIRTUAL_ENV_PROMPT=(Django-chat-app) 
 
 :END
 if defined _OLD_CODEPAGE (
--- chatApp/chatApp/settings.py
+++ chatApp/chatApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x50+!++$2rc&i%8-e54x%gcil$op%@lnd42p36z!j6rh95yp9e'
+SECRET_KEY = 'django-insecure-5u#r2+pmws5a5&cbm3@8gcy2h^w=5lpo5r5u&jimlsxxgy#yen'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
