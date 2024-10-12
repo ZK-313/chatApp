47bf940ccec4a409e66e218c82b7a0fce1fe2d75
zk-313 <zulfiqar991@hotmail.com>
2024-10-11 22:58:45 -0400

    added account functionality

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path("", views.frontpage, name="frontpage"),
+    path("", views.front_test, name="frontpage"),
+    path('signup/', views.signup, name = 'signup'),
+    path('chat/', views.chat, name = 'chat'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
